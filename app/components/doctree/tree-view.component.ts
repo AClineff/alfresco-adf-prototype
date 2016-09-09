b53e882702d882127f47f4e8122f84d26d421a25
Allen Clineff <Allen Clineff>
2016-09-09 14:48:10 -0400

    Updated Code Documentation

--- app/components/doctree/tree-view.component.ts
+++ app/components/doctree/tree-view.component.ts
@@ -28,55 +28,94 @@ import {DocumentListService} from 'ng2-alfresco-documentlist';
     directives: [TreeView]
 })
 export class TreeView {
+    // Nodes in this level of the tree.
     @Input() nodes: Array<any>;
+
+    // ID of node to be highlighted, checked against nodes in this level.
     @Input() highlightedNode: string;
+
+    // Event emitter for when a user clicks on an item
     @Output() selectedNode = new EventEmitter();
 
+    // alfresco DocumentListService service that we use to handle our API calls.
     listService: DocumentListService;
 
+    /**
+     * @constructor
+     * @desc instantiates instance of DocumentListService
+     * @param listService
+     */
     constructor(listService: DocumentListService){
         this.listService = listService;
     }
 
+    /**
+     * This handles a user clicking on folders or items in the tree.
+     * @param node Node clicked.
+     */
     onItemClick(node){
         if( this.isFolder(node)) this.toggle(node);
         else this.selectedNode.emit(node.entry.id);
     }
 
+    /**
+     * This handles children that have been selected, passes it up the tree.
+     * @param nodeId string
+     */
     nodeSelected(nodeId){
         this.selectedNode.emit(nodeId);
     }
 
+    /**
+     * Returns true if the node has children
+     * @param node
+     * @returns {(node:any)=>(node:any)=>any}
+     */
     isFolder(node){
         return node.entry.isFolder;
     }
 
+    /**
+     *
+     * @param node
+     */
     toggle(node){
+        // Collapse expanded folders
         if(node.expanded){
             node.expanded = false;
             return;
         }
 
+        // If it already has a children property, just toggle the expanded property.
         if(node.children){
             node.expanded = true;
             return;
         }
 
+        // Else the node is a folder but has unfetched children, so we fetch them.
+        // We build the path by hand because the Alfresco Path property doesn't actually work.
         let currentPath = '';
         for(let i = 1; i < node.entry.path.elements.length ; i++){
+            //noinspection TypeScriptUnresolvedVariable
             currentPath+= '/' + node.entry.path.elements[i].name;
         }
         let newPath = currentPath + '/' + node.entry.name;
+
+        // getFolder returns Observable, which we subscribe to and handle the response.
         let folderStream = this.listService.getFolder(newPath);
         folderStream.subscribe(
             resp => {
-                console.debug(resp);
                 node.children  = resp.list.entries;
             }
         );
         node.expanded = true;
     }
 
+    /**
+     * There's probably a more angular 2.0 way to accomplish this...
+     * @param node
+     * @returns {string}
+     */
     highlightedClass(node){
         if(this.highlightedNode){
             if(node.entry.id == this.highlightedNode){
